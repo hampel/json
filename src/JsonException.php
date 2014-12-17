4478eee194e105e05d0824ddf31d3aaf397abf64
Simon Hampel <simon@hampelgroup.com>
2014-12-17 13:59:26 +1100

    added new error codes JSON_ERROR_RECURSION, JSON_ERROR_INF_OR_NAN and JSON_ERROR_UNSUPPORTED_TYPE to JsonException

--- src/JsonException.php
+++ src/JsonException.php
@@ -8,7 +8,10 @@ class JsonException extends \Exception
 		JSON_ERROR_STATE_MISMATCH => "Invalid or malformed JSON",
 		JSON_ERROR_CTRL_CHAR => "Control character error, possibly incorrectly encoded",
 		JSON_ERROR_SYNTAX => "Syntax error",
-		JSON_ERROR_UTF8 => "Malformed UTF-8 characters, possibly incorrectly encoded"
+		JSON_ERROR_UTF8 => "Malformed UTF-8 characters, possibly incorrectly encoded",
+		JSON_ERROR_RECURSION => "The object or array passed to json_encode() include recursive references and cannot be encoded",
+		JSON_ERROR_INF_OR_NAN => "The value passed to json_encode() includes either NAN or INF",
+		JSON_ERROR_UNSUPPORTED_TYPE => "A value of an unsupported type was given to json_encode(), such as a resource",
 	];
 
 	public function __construct($message = "", $code = 0, \Exception $previous = null)
