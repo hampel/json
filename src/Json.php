a75ce7deeb80040222c480d4ac79cce9204029a1
Simon Hampel <simon@hampelgroup.com>
2014-12-17 13:20:48 +1100

    added $options parameter to decode, to support PHP 5.4.0 version of json_decode

--- src/Json.php
+++ src/Json.php
@@ -32,14 +32,15 @@ class Json
 	 * @param mixed $data 		The json string being decoded. Only works with UTF-8 encoded data
 	 * @param bool $assoc		When TRUE, returned objects will be converted into associative arrays
 	 * @param int $depth		User specified recursion depth
+	 * @param array $options    Array of option flags
 	 *
 	 * @return string Returns the contents of the JSON encoded string as the appropriate PHP type on success
 	 *
 	 * @throws JsonException
 	 */
-	public static function decode($data, $assoc = false, $depth = 512)
+	public static function decode($data, $assoc = false, $depth = 512, $options = 0)
 	{
-		$decoded_data = @json_decode($data, $assoc, $depth);
+		$decoded_data = @json_decode($data, $assoc, $depth, $options);
 
 		$json_error = json_last_error();
 
