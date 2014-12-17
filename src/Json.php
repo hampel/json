878640f460aaff93474ade9cf457554ce5a46803
Simon Hampel <simon@hampelgroup.com>
2014-12-17 13:01:00 +1100

    remove code not compatible with PHP 5.3

--- src/Json.php
+++ src/Json.php
@@ -7,15 +7,14 @@ class Json
 	 *
 	 * @param mixed $data 	The data being encoded. Can be any type except a resource. Only works with UTF-8 encoded data
 	 * @param int $options	Bitmask of json_encode options
-	 * @param int $depth	Maximum depth
 	 *
 	 * @return string Returns a JSON encoded string on success
 	 *
 	 * @throws JsonException
 	 */
-	public function encode($data, $options = 0, $depth = 512)
+	public static function encode($data, $options = 0)
 	{
-		$json_data = @json_encode($data, $options, $depth);
+		$json_data = @json_encode($data, $options);
 
 		$json_error = json_last_error();
 
@@ -39,9 +38,9 @@ class Json
 	 *
 	 * @throws JsonException
 	 */
-	public function decode($data, $assoc = false, $depth = 512, $options = 0)
+	public static function decode($data, $assoc = false, $depth = 512)
 	{
-		$decoded_data = @json_decode($data, $assoc, $depth, $options);
+		$decoded_data = @json_decode($data, $assoc, $depth);
 
 		$json_error = json_last_error();
 
@@ -51,17 +50,6 @@ class Json
 		}
 
 		return $decoded_data;
-	}
-
-	public static function __callStatic($method, $parameters)
-	{
-		$method_names = array('encode', 'decode');
-
-		if (in_array($method, $method_names))
-		{
-			$instance = new static();
-			call_user_func_array(array($instance, $method), $parameters);
-		}
 	}
 }
 
